bd2aafbc6afbf62efb9f6d12eca16ff105c421cb
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-10 16:59:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -18,6 +18,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY"  
+  end
 
 
 end
