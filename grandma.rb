f55b56cffd7c61b75761bceeb2474127a35c08ad
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-10 17:00:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,5 @@ def speak_to_grandma(phrase)
   elsif (phrase==phrase.upcase)
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP, SONNY"  
-  end
-
-
+    return "HUH?! SPEAK UP, SONNY"
 end
